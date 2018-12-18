bf0438dbcda55cb7b2d49088d4c2530ee7939db3
yunsunyong <ysy9568@naver.com>
2018-12-18 21:32:51 +0900

    과제 2018-12-18
    컬렉션과제

---
+++ boardProject/src/board/model/vo/AscBoardTitle.java
@@ -0,0 +1,18 @@
+package board.model.vo;
+
+import java.util.Comparator;
+
+public class AscBoardTitle implements Comparator{
+	@Override
+	public int compare(Object o1, Object o2) {
+		int result = 0;
+		
+		if(o1 instanceof Board && o2 instanceof Board) {
+			Board b1 = (Board)o1;
+			Board b2 = (Board)o2;
+			
+			result = b1.getBoardTitle().compareTo(b2.getBoardTitle()) > 0 ? 1: (b1.getBoardTitle().compareTo(b2.getBoardTitle()) < 0? -1 : 0); 
+		}
+		return result;
+	}
+}
